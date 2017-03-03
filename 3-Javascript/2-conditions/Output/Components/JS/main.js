57e8d0a31212a8621e2e00a127a678e2817ab1bc
Tee <jan.marvin.o.tee@accenture.com>
2017-03-04 01:13:55 +0800

    javascript 1 2

---
+++ 3-Javascript/2-conditions/Output/Components/JS/main.js
@@ -0,0 +1,19 @@
+ var x = 5; 
+ var y = 6; 
+var z = x + y;
+
+if(z > 6){
+document.getElementById("sum").innerHTML=z;
+}
+
+
+var x = "hello "
+var y ="builtable"
+z = x + y; 
+
+if(z !== ""){
+document.getElementById("text").innerHTML=z;
+}
+
+
+// more here https://www.w3schools.com/js/js_comparisons.asp
