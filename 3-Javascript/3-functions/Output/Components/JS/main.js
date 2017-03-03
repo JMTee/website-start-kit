62f8b2731691e0b0ca21db137e4430e33aa48155
Tee <jan.marvin.o.tee@accenture.com>
2017-03-04 01:31:15 +0800

    javascript conditions

---
+++ 3-Javascript/3-functions/Output/Components/JS/main.js
@@ -0,0 +1,30 @@
+
+
+
+(function () {
+
+ var x = 5; 
+ var y = 6; 
+var z = x + y;
+
+if(z > 6){
+document.getElementById("sum").innerHTML=z;
+}
+
+})();
+
+
+
+function clickFunc(){
+  
+var x = "hello "
+var y ="builtable"
+z = x + y; 
+
+if(z !== ""){
+document.getElementById("text").innerHTML=z;
+}
+
+
+
+}
